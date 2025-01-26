dfa0f8e2de9225db38e74735ad74b6f2bfdbbf97
jtmhuka33 <jtmhuka33@gmail.com>
2025-01-26 15:29:44 +0100

    fix: added introduction responsiveness with avatar

--- src/app/components/Introduction.js
+++ src/app/components/Introduction.js
@@ -8,13 +8,25 @@ function Introduction() {
        mb: 8,
        mt: 8,
        display: 'flex',
-       alignItems: 'center',
-       justifyContent: 'space-between',
+       flexDirection: {xs: 'column', sm: 'row'},
+       alignItems: { xs: 'center', sm: 'center'},
+       justifyContent: {xs: 'center', sm: 'space-between'},
        gap:4,
       }}
     >
-      <Avatar sx={{width: 100, height: 100, border: '1px grey solid'}} alt='Tadiwanashe John Mhuka' src={MyImage.src} />
-      <Typography variant="body1">
+      <Avatar 
+        sx={{
+          width: { sm: 200, xs: 300}, 
+          height: { sm: 200, xs: 300}, 
+          border: '1px grey solid',
+          mb: {xs: 4, sm: 0}
+        }} 
+        alt='Tadiwanashe John Mhuka' 
+        src={MyImage.src} 
+      />
+      <Typography
+        alignContent="center"
+      >
       Fueled by a childhood fascination with software development,
       I'm a web development enthusiast currently studying Interactive Media at the University of Augsburg.
       My passion for building games and websites led me to learn JavaScript, HTML, React, and CSS.
