741669f6547c54d2e145aa1a1b2ad83ecbb2290c
jtmhuka33 <jtmhuka33@gmail.com>
2025-12-14 17:59:05 +0100

    updating description

--- src/app/components/Introduction.js
+++ src/app/components/Introduction.js
@@ -1,7 +1,7 @@
 "use client";
-import { Avatar, Box, Typography } from '@mui/material';
+import {Avatar, Box, Typography} from '@mui/material';
 import MyImage from '../Images/MyImage.webp';
-import { motion } from 'framer-motion';
+import {motion} from 'framer-motion';
 
 // Create motion-enhanced components
 const MotionBox = motion(Box);
@@ -10,66 +10,65 @@ const MotionTypography = motion(Typography);
 
 // Parent container variants for staggering children animations
 const containerVariants = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.3,
+    hidden: {opacity: 0},
+    visible: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.3,
+        },
     },
-  },
 };
 
 // Child variants for a simple fade-in and slide-up effect
 const childVariants = {
-  hidden: { opacity: 0, y: 20 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: { duration: 0.6 },
-  },
+    hidden: {opacity: 0, y: 20},
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {duration: 0.6},
+    },
 };
 
 function Introduction() {
-  return (
-    <MotionBox
-      component="section"
-      sx={{
-        mb: 8,
-        mt: 8,
-        display: 'flex',
-        flexDirection: { xs: 'column', sm: 'row' },
-        alignItems: { xs: 'center', sm: 'center' },
-        justifyContent: { xs: 'center', sm: 'space-between' },
-        gap: 4,
-      }}
-      // Animate on mount
-      initial="hidden"
-      animate="visible"
-      variants={containerVariants}
-    >
-      <MotionAvatar
-        sx={{
-          width: { sm: 175, xs: 300 },
-          height: { sm: 175, xs: 300 },
-          border: '1px grey solid',
-          mb: { xs: 4, sm: 0 },
-        }}
-        alt="Tadiwanashe John Mhuka"
-        src={MyImage.src}
-        variants={childVariants}
-      />
-      <MotionTypography
-        alignContent="center"
-        variants={childVariants}
-      >
-        Fueled by a childhood fascination with software development,
-        I studied Interactive Media at the University of Applied Sciences Augsburg.
-        My passion for building games and websites led me to learn JavaScript, HTML, React, and CSS and others.
-        With experience in remote work and a knack for tackling diverse tasks while maintaining balance,
-        I'm eager to contribute my skills to your team.
-      </MotionTypography>
-    </MotionBox>
-  );
+    return (
+        <MotionBox
+            component="section"
+            sx={{
+                mb: 8,
+                mt: 8,
+                display: 'flex',
+                flexDirection: {xs: 'column', sm: 'row'},
+                alignItems: {xs: 'center', sm: 'center'},
+                justifyContent: {xs: 'center', sm: 'space-between'},
+                gap: 4,
+            }}
+            // Animate on mount
+            initial="hidden"
+            animate="visible"
+            variants={containerVariants}
+        >
+            <MotionAvatar
+                sx={{
+                    width: {sm: 175, xs: 300},
+                    height: {sm: 175, xs: 300},
+                    border: '1px grey solid',
+                    mb: {xs: 4, sm: 0},
+                }}
+                alt="Tadiwanashe John Mhuka"
+                src={MyImage.src}
+                variants={childVariants}
+            />
+            <MotionTypography
+                alignContent="center"
+                variants={childVariants}
+            >
+                Fueled by a childhood fascination with software development,
+                I studied Interactive Media at the University of Applied Sciences Augsburg.
+                My passion for building games and websites led me to learn JavaScript, HTML, React, and CSS and others
+                and I'm eager to contribute my skills to your team.
+            </MotionTypography>
+        </MotionBox>
+    );
 }
 
 export default Introduction;
